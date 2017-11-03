batch normalization fixed

ResNet.py
@@ -18,40 +18,41 @@ from ops import *
 from utils import *
 from network import create_network
 
-class ResNet:
 
+class ResNet:
     # Class properties
-    __network = None         # Graph for ResNet
-    __train_op = None        # Operation used to optimize loss function
-    __loss = None            # Loss function to be optimized, which is based on predictions
-    __accuracy = None        # Classification accuracy for all conditions
-    __probs = None           # Prediction probability matrix of shape [batch_size, numClasses]
+    __network = None  # Graph for ResNet
+    __train_op = None  # Operation used to optimize loss function
+    __loss = None  # Loss function to be optimized, which is based on predictions
+    __accuracy = None  # Classification accuracy for all conditions
+    __probs = None  # Prediction probability matrix of shape [batch_size, numClasses]
 
     def __init__(self, numClasses, imgSize, imgChannel):
         self.imgSize = imgSize
         self.imgChannel = imgChannel
         self.numClasses = numClasses
-        self.h = 100            # number of hidden units of the fully-connected layer
-        self.lmbda = 5e-04      # weight decay
-        self.init_lr = 0.001    # initial learning rate
+        self.h = 100  # number of hidden units of the fully-connected layer
+        self.lmbda = 5e-04  # weight decay
+        self.init_lr = 0.001  # initial learning rate
         self.x, self.y, self.keep_prob = self.create_placeholders()
+        self.is_train = tf.Variable(True, trainable=False, dtype=tf.bool)
 
     def create_placeholders(self):
         with tf.name_scope('Input'):
-            self.x = tf.placeholder(tf.float32, shape=(None,
-                                                       self.imgSize,
-                                                       self.imgSize,
-                                                       self.imgChannel), name='x-input')
-            self.y = tf.placeholder(tf.float32, shape=(None, self.numClasses), name='y-input')
-            self.keep_prob = tf.placeholder(tf.float32)
-        return self.x, self.y, self.keep_prob
+            x = tf.placeholder(tf.float32, shape=(None,
+                                                  self.imgSize,
+                                                  self.imgSize,
+                                                  self.imgChannel), name='x-input')
+            y = tf.placeholder(tf.float32, shape=(None, self.numClasses), name='y-input')
+            keep_prob = tf.placeholder(tf.float32)
+        return x, y, keep_prob
 
     def inference(self):
         if self.__network:
             return self
         # Building network...
         with tf.variable_scope('ResNet'):
-            net = create_network(self.x, self.h, self.keep_prob, self.numClasses)
+            net = create_network(self.x, self.h, self.keep_prob, self.numClasses, self.is_train)
         self.__network = net
         return self
 
@@ -76,7 +77,8 @@ class ResNet:
                 cross_entropy = cross_entropy_loss(self.y, self.__network)
                 tf.summary.scalar('cross_entropy', cross_entropy)
             with tf.name_scope('l2_loss'):
-                l2_loss = tf.reduce_sum(self.lmbda * tf.stack([tf.nn.l2_loss(v) for v in tf.get_collection('reg_weights')]))
+                l2_loss = tf.reduce_sum(
+                    self.lmbda * tf.stack([tf.nn.l2_loss(v) for v in tf.get_collection('reg_weights')]))
                 tf.summary.scalar('l2_loss', l2_loss)
             with tf.name_scope('total'):
                 self.__loss = cross_entropy + l2_loss

network.py
@@ -13,18 +13,28 @@ Comments: Includes function which creates the ResNet with 50 layer.
 **********************************************************************************
 """
 
-from ops import conv_layer, bottleneck_block, avg_pool, flatten_layer, fc_layer, dropout
+from ops import conv_2d, bottleneck_block, avg_pool, flatten_layer, fc_layer, dropout
 import tensorflow as tf
 
 
-def create_network(X, h, keep_prob, numClasses):
-    res1 = conv_layer(X,
-                      layer_name='res1',
-                      stride=2,
-                      filter_size=4,
-                      num_filters=32,
-                      batch_norm=True,
-                      use_relu=True)
+def create_network(X, h, keep_prob, numClasses, is_train):
+    """
+    Building the Residual Network with 50 layer
+    :param X: input
+    :param h: number of units in the fully connected layer
+    :param keep_prob: dropout rate
+    :param numClasses: number of classes
+    :param is_train: to be used by batch normalization
+    :return:
+    """
+    res1 = conv_2d(X,
+                   layer_name='res1',
+                   stride=2,
+                   filter_size=4,
+                   num_filters=32,
+                   is_train=is_train,
+                   batch_norm=True,
+                   use_relu=True)
 
     # res1 = max_pool(res1, ksize=2, stride=2, name='res1_max_pool')
     print('---------------------')
@@ -33,7 +43,7 @@ def create_network(X, h, keep_prob, numClasses):
     print('---------------------')
     # Res2
     with tf.variable_scope('Res2'):
-        res2a = bottleneck_block(res1, block_name='res2a',
+        res2a = bottleneck_block(res1, is_train, block_name='res2a',
                                  s1=1, k1=1, nf1=32, name1='res2a_branch2a',
                                  s2=1, k2=3, nf2=32, name2='res2a_branch2b',
                                  s3=1, k3=1, nf3=64, name3='res2a_branch2c',
@@ -41,7 +51,7 @@ def create_network(X, h, keep_prob, numClasses):
         print('Res2a')
         print(res2a.get_shape())
         print('---------------------')
-        res2b = bottleneck_block(res2a, block_name='res2b',
+        res2b = bottleneck_block(res2a, is_train, block_name='res2b',
                                  s1=1, k1=1, nf1=32, name1='res2b_branch2a',
                                  s2=1, k2=3, nf2=32, name2='res2b_branch2b',
                                  s3=1, k3=1, nf3=64, name3='res2b_branch2c',
@@ -49,7 +59,7 @@ def create_network(X, h, keep_prob, numClasses):
         print('Res2b')
         print(res2b.get_shape())
         print('---------------------')
-        res2c = bottleneck_block(res2b, block_name='res2c',
+        res2c = bottleneck_block(res2b, is_train, block_name='res2c',
                                  s1=1, k1=1, nf1=32, name1='res2c_branch2a',
                                  s2=1, k2=3, nf2=32, name2='res2c_branch2b',
                                  s3=1, k3=1, nf3=64, name3='res2c_branch2c',
@@ -60,7 +70,7 @@ def create_network(X, h, keep_prob, numClasses):
 
     # Res3
     with tf.variable_scope('Res3'):
-        res3a = bottleneck_block(res2c, block_name='res3a',
+        res3a = bottleneck_block(res2c, is_train, block_name='res3a',
                                  s1=2, k1=1, nf1=48, name1='res3a_branch2a',
                                  s2=1, k2=3, nf2=48, name2='res3a_branch2b',
                                  s3=1, k3=1, nf3=128, name3='res3a_branch2c',
@@ -68,7 +78,7 @@ def create_network(X, h, keep_prob, numClasses):
         print('Res3a')
         print(res3a.get_shape())
         print('---------------------')
-        res3b = bottleneck_block(res3a, block_name='res3b',
+        res3b = bottleneck_block(res3a, is_train, block_name='res3b',
                                  s1=1, k1=1, nf1=48, name1='res3b_branch2a',
                                  s2=1, k2=3, nf2=48, name2='res3b_branch2b',
                                  s3=1, k3=1, nf3=128, name3='res3b_branch2c',
@@ -76,7 +86,7 @@ def create_network(X, h, keep_prob, numClasses):
         print('Res3b')
         print(res3b.get_shape())
         print('---------------------')
-        res3c = bottleneck_block(res3b, block_name='res3c',
+        res3c = bottleneck_block(res3b, is_train, block_name='res3c',
                                  s1=1, k1=1, nf1=48, name1='res3c_branch2a',
                                  s2=1, k2=3, nf2=48, name2='res3c_branch2b',
                                  s3=1, k3=1, nf3=128, name3='res3c_branch2c',
@@ -84,7 +94,7 @@ def create_network(X, h, keep_prob, numClasses):
         print('Res3c')
         print(res3c.get_shape())
         print('---------------------')
-        res3d = bottleneck_block(res3c, block_name='res3d',
+        res3d = bottleneck_block(res3c, is_train, block_name='res3d',
                                  s1=1, k1=1, nf1=48, name1='res3d_branch2a',
                                  s2=1, k2=3, nf2=48, name2='res3d_branch2b',
                                  s3=1, k3=1, nf3=128, name3='res3d_branch2c',
@@ -95,7 +105,7 @@ def create_network(X, h, keep_prob, numClasses):
 
     # Res4
     with tf.variable_scope('Res4'):
-        res4a = bottleneck_block(res3d, block_name='res4a',
+        res4a = bottleneck_block(res3d, is_train, block_name='res4a',
                                  s1=2, k1=1, nf1=64, name1='res4a_branch2a',
                                  s2=1, k2=3, nf2=64, name2='res4a_branch2b',
                                  s3=1, k3=1, nf3=256, name3='res4a_branch2c',
@@ -104,7 +114,7 @@ def create_network(X, h, keep_prob, numClasses):
         print('Res4a')
         print(res4a.get_shape())
         print('---------------------')
-        res4b = bottleneck_block(res4a, block_name='res4b',
+        res4b = bottleneck_block(res4a, is_train, block_name='res4b',
                                  s1=1, k1=1, nf1=64, name1='res4b_branch2a',
                                  s2=1, k2=3, nf2=64, name2='res4b_branch2b',
                                  s3=1, k3=1, nf3=256, name3='res4b_branch2c',
@@ -112,7 +122,7 @@ def create_network(X, h, keep_prob, numClasses):
         print('Res4b')
         print(res4b.get_shape())
         print('---------------------')
-        res4c = bottleneck_block(res4b, block_name='res4c',
+        res4c = bottleneck_block(res4b, is_train, block_name='res4c',
                                  s1=1, k1=1, nf1=64, name1='res4c_branch2a',
                                  s2=1, k2=3, nf2=64, name2='res4c_branch2b',
                                  s3=1, k3=1, nf3=256, name3='res4c_branch2c',
@@ -120,7 +130,7 @@ def create_network(X, h, keep_prob, numClasses):
         print('Res4c')
         print(res4c.get_shape())
         print('---------------------')
-        res4d = bottleneck_block(res4c, block_name='res4d',
+        res4d = bottleneck_block(res4c, is_train, block_name='res4d',
                                  s1=1, k1=1, nf1=64, name1='res4d_branch2a',
                                  s2=1, k2=3, nf2=64, name2='res4d_branch2b',
                                  s3=1, k3=1, nf3=256, name3='res4d_branch2c',
@@ -128,7 +138,7 @@ def create_network(X, h, keep_prob, numClasses):
         print('Res4d')
         print(res4d.get_shape())
         print('---------------------')
-        res4e = bottleneck_block(res4d, block_name='res4e',
+        res4e = bottleneck_block(res4d, is_train, block_name='res4e',
                                  s1=1, k1=1, nf1=64, name1='res4e_branch2a',
                                  s2=1, k2=3, nf2=64, name2='res4e_branch2b',
                                  s3=1, k3=1, nf3=256, name3='res4e_branch2c',
@@ -136,7 +146,7 @@ def create_network(X, h, keep_prob, numClasses):
         print('Res4e')
         print(res4e.get_shape())
         print('---------------------')
-        res4f = bottleneck_block(res4e, block_name='res4f',
+        res4f = bottleneck_block(res4e, is_train, block_name='res4f',
                                  s1=1, k1=1, nf1=64, name1='res4f_branch2a',
                                  s2=1, k2=3, nf2=64, name2='res4f_branch2b',
                                  s3=1, k3=1, nf3=256, name3='res4f_branch2c',
@@ -147,7 +157,7 @@ def create_network(X, h, keep_prob, numClasses):
 
     # Res5
     with tf.variable_scope('Res5'):
-        res5a = bottleneck_block(res4f, block_name='res5a',
+        res5a = bottleneck_block(res4f, is_train, block_name='res5a',
                                  s1=1, k1=1, nf1=128, name1='res5a_branch2a',
                                  s2=1, k2=3, nf2=128, name2='res5a_branch2b',
                                  s3=1, k3=1, nf3=512, name3='res5a_branch2c',
@@ -156,7 +166,7 @@ def create_network(X, h, keep_prob, numClasses):
         print('Res5a')
         print(res5a.get_shape())
         print('---------------------')
-        res5b = bottleneck_block(res5a, block_name='res5b',
+        res5b = bottleneck_block(res5a, is_train, block_name='res5b',
                                  s1=1, k1=1, nf1=128, name1='res5b_branch2a',
                                  s2=1, k2=3, nf2=128, name2='res5b_branch2b',
                                  s3=1, k3=1, nf3=512, name3='res5b_branch2c',
@@ -164,7 +174,7 @@ def create_network(X, h, keep_prob, numClasses):
         print('Res5b')
         print(res5b.get_shape())
         print('---------------------')
-        res5c = bottleneck_block(res5b, block_name='res5c',
+        res5c = bottleneck_block(res5b, is_train, block_name='res5c',
                                  s1=1, k1=1, nf1=128, name1='res5c_branch2a',
                                  s2=1, k2=3, nf2=128, name2='res5c_branch2b',
                                  s3=1, k3=1, nf3=512, name3='res5c_branch2c',
@@ -178,13 +188,12 @@ def create_network(X, h, keep_prob, numClasses):
         print(res5c.get_shape())
         print('---------------------')
 
-    net_flatten, _ = flatten_layer(res5c)
+    net_flatten = flatten_layer(res5c)
     print('---------------------')
     print('Matrix dimension to the first FC layer')
     print(net_flatten.get_shape())
     print('---------------------')
-    net = fc_layer(net_flatten, h, 'FC1', batch_norm=True, add_reg=True, use_relu=True)
+    net = fc_layer(net_flatten, h, 'FC1', is_train=is_train, batch_norm=True, add_reg=True, use_relu=True)
     net = dropout(net, keep_prob)
-    net = fc_layer(net, numClasses, 'FC2', batch_norm=True, add_reg=True, use_relu=False)
-
+    net = fc_layer(net, numClasses, 'FC2', is_train=is_train, batch_norm=True, add_reg=True, use_relu=False)
     return net

train.py
@@ -65,9 +65,9 @@ def train(image_size=28,
                 start = step * batch_size
                 end = (step + 1) * batch_size
                 X_batch, Y_batch = get_next_batch(X_train, Y_train, start, end)
-                # X_batch = random_rotation_2d(X_batch, 20.0)
+                X_batch = random_rotation_2d(X_batch, 90.0)
+                model.is_train = True
                 feed_dict_batch = {model.x: X_batch, model.y: Y_batch, model.keep_prob: 0.5}
-
                 _, acc_batch, loss_batch = sess.run([model.train_op, model.accuracy, model.loss],
                                                     feed_dict=feed_dict_batch)
                 acc_batch_all = np.append(acc_batch_all, acc_batch)
@@ -88,6 +88,7 @@ def train(image_size=28,
                     sum_count += 1
                     acc_batch_all = loss_batch_all = np.array([])
 
+            model.is_train = False
             feed_dict_val = {model.x: X_valid, model.y: Y_valid, model.keep_prob: 1}
             acc_valid, loss_valid = sess.run([model.accuracy, model.loss], feed_dict=feed_dict_val)
             summary_valid = tf.Summary(value=[tf.Summary.Value(tag='Accuracy', simple_value=acc_valid)])